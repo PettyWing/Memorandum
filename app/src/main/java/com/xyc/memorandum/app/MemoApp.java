84e18db02ff2fe87f56d1598010ccc4b476375c8
PettyWing <1612834907@qq.com>
2017-02-21 16:33:34 +0800

    add all

---
+++ app/src/main/java/com/xyc/memorandum/app/MemoApp.java
@@ -0,0 +1,9 @@
+package com.xyc.memorandum.app;
+
+import android.app.Application;
+
+/**
+ * Created by XieYucheng on 2017/2/21.
+ */
+public class MemoApp extends Application{
+}
---
+++ app/src/main/java/com/xyc/memorandum/app/activity/MemoListActivity.java
@@ -0,0 +1,7 @@
+package com.xyc.memorandum.app.activity;
+
+/**
+ * Created by XieYucheng on 2017/2/21.
+ */
+public class MemoListActivity {
+}
